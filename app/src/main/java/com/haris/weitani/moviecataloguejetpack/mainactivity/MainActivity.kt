ce9939368b357a6e55ea8db3cb39776bc28a4254
sirwarfox <hweitani@gmail.com>
2020-01-18 21:35:53 +0700

    Add Favorite on app bar

--- app/src/main/java/com/haris/weitani/moviecataloguejetpack/mainactivity/MainActivity.kt
+++ app/src/main/java/com/haris/weitani/moviecataloguejetpack/mainactivity/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.haris.weitani.moviecataloguejetpack.mainactivity
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.haris.weitani.moviecataloguejetpack.MainSectionPagerAdapter
 import com.haris.weitani.moviecataloguejetpack.R
+import com.haris.weitani.moviecataloguejetpack.favoriteactivity.FavoriteActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +32,19 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_favorite -> {
+                startActivity(intentFor<FavoriteActivity>())
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
